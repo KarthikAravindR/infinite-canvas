4910fd37ccbd6ab552c9c7c4f355790bcd5fba0c
Karthik Aravind <karthikaravindpmc@gmail.com>
2024-04-09 19:49:30 +0530

    added d3 to external

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
       fileName: (format) => `react-infinite-canvas.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'd3-zoom', 'd3-selection'],
       output: {
         globals: {
           react: 'React',
